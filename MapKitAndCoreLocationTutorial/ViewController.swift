21fe6c92a424aab952c71bec2597f42e84410dbb
Alex Rybachev <rybachev.as@gmail.com>
2022-05-06 22:05:26 +0400

    Initial Commit

---
+++ MapKitAndCoreLocationTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapKitAndCoreLocationTutorial
+//
+//  Created by Aleksandr Rybachev on 06.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
