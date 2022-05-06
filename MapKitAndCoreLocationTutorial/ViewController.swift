1351259f6c03dc69c6bac1f4428ba1ce55677217
Alex Rybachev <rybachev.as@gmail.com>
2022-05-06 22:31:48 +0400

    подготовка по уроку

--- MapKitAndCoreLocationTutorial/ViewController.swift
+++ MapKitAndCoreLocationTutorial/ViewController.swift
@@ -5,15 +5,89 @@
 //  Created by Aleksandr Rybachev on 06.05.2022.
 //
 
+/*
+ Проект сделан по уроку https://youtu.be/5fWG5ofhHsY
+ */
+
 import UIKit
+import MapKit
+import CoreLocation
+import AVFoundation
+//import Layoutless
 
 class ViewController: UIViewController {
-
+    
+    lazy var directionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Where do you want to go?"
+        label.font = .boldSystemFont(ofSize: 16)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    lazy var textfield: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Enter your destination"
+        tf.borderStyle = .roundedRect
+        return tf
+    }()
+    
+    lazy var getDirectionButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Get Direction", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 16)
+        button.addTarget(self, action: #selector(getDirectionButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var startStopButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Start Navigation", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 16)
+        button.addTarget(self, action: #selector(startStopButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var mapView: MKMapView = {
+        let mapView = MKMapView()
+        return mapView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupViews()
     }
-
-
+    
+    @objc fileprivate func getDirectionButtonTapped() {
+        
+    }
+    
+    @objc fileprivate func startStopButtonTapped() {
+        
+    }
+    
+    fileprivate func setupViews() {
+        view.backgroundColor = .systemBackground
+        
+        stack(.vertical) (
+            directionLabel.insetting(by: 16),
+            stack(.horizontal, spacing: 16)(
+                textfield,
+                getDirectionButton
+            ).insetting(by: 16),
+            startStopButton.insetting(by: 16),
+            mapView
+        ).fillingParent(relativeToSafeArea: true).layout(in: view)
+    }
+    
+    fileprivate func centerViewToUserLocation(center: CLLocationCoordinate2D) {
+        
+    }
+    
+    fileprivate func handleAuthorizationStatus(locationManager: CLLocationManager, status: CLAuthorizationStatus) {
+        
+    }
 }
-
